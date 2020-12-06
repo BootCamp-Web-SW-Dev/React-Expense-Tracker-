d9662ca362c03497e173b93fa611bc6573f0881c
Faraz Idris Khan <fikhan00@gmail.com>
2020-12-06 14:34:27 +0300

    Final UI Changes 3
    Final Changes 3

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -8,10 +8,9 @@ export const AccountSummary = () => {
          .filter(transaction => transaction > 0)
          .reduce((acc, transaction) => (acc += transaction), 0)
          .toFixed(2);
-    const expense = transactionAmounts
+    const expense = Math.abs(transactionAmounts
           .filter(transaction => transaction > 0)
-          .reduce((acc,transaction) => ( acc += transaction),0)
-          .toFixed(2);
+          .reduce((acc,transaction) => ( acc += transaction),0)).toFixed(2);
     return (
         <div className="inc-exp-container">
             <div>
