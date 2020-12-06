f800da8e2a2f25a135a304107812ce99afca56da
Faraz Idris Khan <fikhan00@gmail.com>
2020-12-06 14:21:54 +0300

    Final UI Changes
    Final UI Changes

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import AppReducer from './AppReducer';
 
 // Create initial state 
 const initialState = {
-    transactions: [
-            {id: 1, description: "Income 1", transactionAmount: 1000},
-            {id: 2, description: "Expense 1", transactionAmount: -100},
-            {id: 3, description: "Income 2", transactionAmount: 2000},
-            {id: 4, description: "Expense 1", transactionAmount: -500}
-    ]
+    transactions: []
 }
 
 // Create the global context
@@ -17,11 +12,31 @@ export const GlobalContext = createContext(initialState);
 
 // Create a provider for the Global context
 export const GlobalProvider = ({ children }) => {
-    const [state] = useReducer(AppReducer,initialState);
+    const [state,dispatch] = useReducer(AppReducer,initialState);
+
+    //Actions for Transactions
+    function delTransaction(id){
+         dispatch({
+             type: 'DELETE_TRANSACTION',
+             payload: id
+         });
+    }
+    
+    //Add new transaction action 
+    function addTransaction(transaction){
+         dispatch({
+             type: 'ADD_TRANSACTION',
+             payload: transaction
+         })
+    }
+
+
     return (
         <GlobalContext.Provider value={
                 {
-                    transactions: state.transactions
+                    transactions: state.transactions,
+                    delTransaction,
+                    addTransaction
                 }
         }>
              {children}
